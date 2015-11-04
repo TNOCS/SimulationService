1a5778a2e79c72be212b7b34b347ccf3edcc6267
Erik Vullings <erik.vullings@gmail.com>
2015-11-04 07:48:28 +0100

    Add script to start all services at once.

---
+++ startall.bat
@@ -0,0 +1,7 @@
+start nodemon SimulationManager/server.js
+start nodemon FloodSim/server.js
+start nodemon CriticalObjectSim/server.js
+start nodemon ElectricalNetworkSim/server.js
+start nodemon HazardousObjectSim/server.js
+start nodemon RoadSim/server.js
+start nodemon CommunicationSim/server.js
